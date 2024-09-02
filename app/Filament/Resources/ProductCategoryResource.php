118155425170ba323e48bca6bc6d323d9d53641b
Md Asaduzzaman <asaduzzaman.arob@gmail.com>
2024-09-03 00:25:43 +0600

    update: theme, product category form and table

--- app/Filament/Resources/ProductCategoryResource.php
+++ app/Filament/Resources/ProductCategoryResource.php
@@ -2,16 +2,23 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\ProductCategoryResource\Pages;
-use App\Filament\Resources\ProductCategoryResource\RelationManagers;
-use App\Models\ProductCategory;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Illuminate\Support\Str;
+use App\Models\ProductCategory;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ProductCategoryResource\Pages;
+use App\Filament\Resources\ProductCategoryResource\RelationManagers;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
 
 class ProductCategoryResource extends Resource
 {
@@ -19,11 +26,37 @@ class ProductCategoryResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Grid::make(2) // 2-column grid
+                    ->schema([
+                        TextInput::make('title')
+                            ->required()
+                            ->maxLength(255)
+                            ->reactive()
+                            ->debounce(500) // Add a debounce delay of 500ms
+                            ->afterStateUpdated(fn($state, callable $set) => $set('slug', Str::slug($state))),
+
+                        TextInput::make('slug')
+                            ->required()
+                            ->unique(ProductCategory::class, 'slug')
+                            ->maxLength(255),
+                    ]),
+
+                Select::make('parent_id')
+                    ->label('Parent Category')
+                    ->relationship('parent', 'title')
+                    ->searchable()
+                    ->preload()
+                    ->nullable()
+                    ->columnSpanFull(),
+
+                Toggle::make('is_active')
+                    ->label('Is Active')
+                    ->default(true)
             ]);
     }
 
@@ -31,13 +64,33 @@ class ProductCategoryResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('title')
+                    ->label('Title')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('slug')
+                    ->label('Slug')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('parent.title')
+                    ->label('parent Category')
+                    ->sortable()
+                    ->searchable(),
+
+                IconColumn::make('is_active')
+                    ->label('Active')
+                    ->boolean()
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle'),
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class ProductCategory extends Model
 {
     use HasFactory;
+
+    public function parent()
+    {
+        return $this->belongsTo(ProductCategory::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(ProductCategory::class, 'parent_id');
+    }
 }
